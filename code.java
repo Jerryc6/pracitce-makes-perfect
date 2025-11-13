4a70331f497a65d74c74e99b2b458e45552257ca
Jerryc6 <jerryclanuevo27@gmail.com>
2025-11-13 01:30:09 -0500

    create login function

--- code.java
+++ code.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class code{
+	
+	public void login(){
+	  /*in this block is the function BLOCK*/
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("LOGIN");
+
+	  String user = "Jerryc";
+	  System.out.println("Username: ");
+	  String input = sc.nextLine();
+	  
+	  String key = "1234";
+	  System.out.println("Password: ");
+	  String pass = sc.nextLine();
+
+	  String in = "Successfull";
+
+	  if(user.equals(input) && pass.equals(key)){
+	  	System.out.println(in);
+	  }else{
+	  	System.out.println("exit(0)");
+	  }
+
+	}
+	
 	public static void main (String[]args){
-		System.out.println("Hello Jerryc");
+		String createAcc = "Don't have account? Press [+] to create";
+		System.out.println("Hello Client "+createAcc);
+		code c = new code();
+		c.login();	
 	}
 }
